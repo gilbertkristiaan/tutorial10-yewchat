85ef21d6b7d26bc1d5abffe507b3a32303a2b60b
gilbertkristiaan <gilbertkristian79@gmail.com>
2025-05-22 12:32:52 +0700

    Experiment 3.2: Be Creative

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ### Original Code
 ![SS 1](images/1.png)
+
+### Be Creative
+![SS 2](images/2.png)
+
+I've changed the main background and sidebar to shades of green, added border effects to the avatars and online status, also changed the text and button colors to harmonious variations of green to make the overall look more natural.
--- src/components/chat.rs
+++ src/components/chat.rs
@@ -139,22 +139,22 @@ impl Component for Chat {
         let submit = ctx.link().callback(|_| Msg::SubmitMessage);
 
         html! {
-            <div class="flex w-screen">
-                <div class="flex-none w-56 h-screen bg-gray-100">
-                    <div class="text-xl p-3">{"Users"}</div>
+            <div class="flex w-screen bg-[#e6f4ea]">
+                <div class="flex-none w-56 h-screen bg-[#d0e8d0]">
+                    <div class="text-xl p-3 font-semibold text-[#3a5a40]">{"Users"}</div>
                     {
                         self.users.clone().iter().map(|u| {
                             html!{
-                                <div class="flex m-3 bg-white rounded-lg p-2">
+                                <div class="flex m-3 bg-white rounded-lg p-2 shadow-sm">
                                     <div>
-                                        <img class="w-12 h-12 rounded-full" src={u.avatar.clone()} alt="avatar"/>
+                                        <img class="w-12 h-12 rounded-full ring-2 ring-[#a3c293]" src={u.avatar.clone()} alt="avatar"/>
                                     </div>
                                     <div class="flex-grow p-3">
-                                        <div class="flex text-xs justify-between">
+                                        <div class="flex text-xs justify-between font-semibold text-[#3a5a40]">
                                             <div>{u.name.clone()}</div>
                                         </div>
-                                        <div class="text-xs text-gray-400">
-                                            {"Hi there!"}
+                                        <div class="text-xs text-green-700 font-medium">
+                                            {"● Online"}
                                         </div>
                                     </div>
                                 </div>
@@ -163,21 +163,23 @@ impl Component for Chat {
                     }
                 </div>
                 <div class="grow h-screen flex flex-col">
-                    <div class="w-full h-14 border-b-2 border-gray-300"><div class="text-xl p-3">{"💬 Chat!"}</div></div>
-                    <div class="w-full grow overflow-auto border-b-2 border-gray-300">
+                    <div class="w-full h-14 border-b-2 border-[#a3c293] bg-[#c9e4c5]">
+                        <div class="text-xl p-3 font-bold text-[#3a5a40]">{"💬 Chat!"}</div>
+                    </div>
+                    <div class="w-full grow overflow-auto border-b-2 border-[#a3c293] px-4 py-2 bg-[#e6f4ea]">
                         {
                             self.messages.iter().map(|m| {
                                 let user = self.users.iter().find(|u| u.name == m.from).unwrap();
                                 html!{
-                                    <div class="flex items-end w-3/6 bg-gray-100 m-8 rounded-tl-lg rounded-tr-lg rounded-br-lg ">
-                                        <img class="w-8 h-8 rounded-full m-3" src={user.avatar.clone()} alt="avatar"/>
+                                    <div class="flex items-end w-3/6 bg-[#c9e4c5] m-4 rounded-lg shadow-sm">
+                                        <img class="w-8 h-8 rounded-full m-3 ring-1 ring-[#8abf7f]" src={user.avatar.clone()} alt="avatar"/>
                                         <div class="p-3">
-                                            <div class="text-sm">
+                                            <div class="text-sm text-[#3a5a40] font-semibold">
                                                 {m.from.clone()}
                                             </div>
-                                            <div class="text-xs text-gray-500">
+                                            <div class="text-xs text-green-800">
                                                 if m.message.ends_with(".gif") {
-                                                    <img class="mt-3" src={m.message.clone()}/>
+                                                    <img class="mt-3 rounded-md" src={m.message.clone()}/>
                                                 } else {
                                                     {m.message.clone()}
                                                 }
@@ -187,11 +189,10 @@ impl Component for Chat {
                                 }
                             }).collect::<Html>()
                         }
-
                     </div>
-                    <div class="w-full h-14 flex px-3 items-center">
-                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700" name="message" required=true />
-                        <button onclick={submit} class="p-3 shadow-sm bg-blue-600 w-10 h-10 rounded-full flex justify-center items-center color-white">
+                    <div class="w-full h-14 flex px-3 items-center bg-[#c9e4c5] border-t border-[#a3c293]">
+                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-[#e6f4ea] rounded-full outline-none text-[#3a5a40] focus:ring-2 focus:ring-[#8abf7f]" name="message" required=true />
+                        <button onclick={submit} class="p-3 shadow-sm bg-[#7caf6f] w-10 h-10 rounded-full flex justify-center items-center text-white hover:bg-[#689459] transition">
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
                                 <path d="M0 0h24v24H0z" fill="none"></path><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
                             </svg>
